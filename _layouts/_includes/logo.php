8c3bb0289f404cd9c9fae3b137c467705d3e64de
Nathan Ryan <55766947+nsryan2@users.noreply.github.com>
2020-06-03 20:15:20 -0500

    Add files via upload

---
+++ _layouts/_includes/logo.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<div class="container">
+        <div class="row">
+            <div class="col-md-5">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+        <div class="row web-header">
+            <div class="col-md-12 col-md-offset-1 header-text">
+                <h1>2022 ANS Student Conference</h1>
+                <h2>Saving the World One Atom at a Time</h2>
+            </div>
+        </div>
+    </div>
+</html>
